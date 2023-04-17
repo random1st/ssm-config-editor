7ef6e8dd43383545338727010f93fceaa3374baf
Roman Voronin <roman@giggster.com>
2023-04-18 00:55:44 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/random1st/ssm-config-editor
+
+go 1.20
+
+require (
+	github.com/aws/aws-sdk-go v1.44.245
+	gopkg.in/yaml.v2 v2.2.8
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/spf13/cobra v1.7.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
---
+++ internal/ssmutil/format.go
@@ -0,0 +1,76 @@
+package ssmutil
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+// isValidJSON checks if the given string is valid JSON
+func IsValidJSON(data string) bool {
+	var js json.RawMessage
+	return json.Unmarshal([]byte(data), &js) == nil
+}
+
+// isValidYAML checks if the given string is valid YAML
+func IsValidYAML(data string) bool {
+	var ym map[string]interface{}
+	err := yaml.Unmarshal([]byte(data), &ym)
+	fmt.Println("err", err)
+	return err == nil
+}
+
+// isValidENV checks if the given string is valid ENV format
+func IsValidENV(data string) bool {
+	lines := strings.Split(data, "\n")
+	for _, line := range lines {
+		if len(line) == 0 || strings.HasPrefix(line, "#") {
+			continue
+		}
+		if !strings.Contains(line, "=") {
+			return false
+		}
+	}
+	return true
+}
+
+func DetectFormat(content []byte) string {
+	if json.Valid(content) {
+		return "json"
+	}
+
+	if IsValidYAML(string(content)) {
+		return "yaml"
+	}
+
+	if IsValidENV(string(content)) {
+		return "env"
+	}
+
+	return "text"
+}
+
+func ValidateFormat(content []byte, format string) error {
+	switch strings.ToLower(format) {
+	case "json":
+		if !json.Valid(content) {
+			return fmt.Errorf("invalid json")
+		}
+	case "yaml":
+		if !IsValidYAML(string(content)) {
+			return fmt.Errorf("invalid yaml")
+
+		}
+	case "env":
+		if !IsValidENV(string(content)) {
+			return fmt.Errorf("invalid env")
+		}
+	case "":
+		// No format specified, skip validation
+	default:
+		return fmt.Errorf("invalid format")
+	}
+	return nil
+}
